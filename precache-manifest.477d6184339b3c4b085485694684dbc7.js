68467f84841bf7faf03c16b549df1ab2973d10a5
alishahwee <alisha.hwee@outlook.com>
2020-06-09 12:51:05 -0500

    Updates

---
+++ precache-manifest.477d6184339b3c4b085485694684dbc7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "feb6add283e82dc3bea7f16054458fbe",
+    "url": "/markdown-previewer/index.html"
+  },
+  {
+    "revision": "27356771f0f7c5bd9691",
+    "url": "/markdown-previewer/static/css/main.da4b428f.chunk.css"
+  },
+  {
+    "revision": "1e28e877dd81c969cc21",
+    "url": "/markdown-previewer/static/js/2.a0d869fa.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/markdown-previewer/static/js/2.a0d869fa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "27356771f0f7c5bd9691",
+    "url": "/markdown-previewer/static/js/main.9383d10e.chunk.js"
+  },
+  {
+    "revision": "35deca1419128cdfce54",
+    "url": "/markdown-previewer/static/js/runtime-main.5fc24b94.js"
+  }
+]);
